1ff97dbad281e6755d1cf70841e07ed3c9fb715d
agas007 <agas9403@gmail.com>
2024-06-08 00:39:26 +0700

    add dashboard page and update for app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Next.js Homepage",
+};
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <h1>
+      Hello, Next.js! <Link href="/dashboard">Dashboard</Link>
+    </h1>
+  );
 }
